f5f2231921b72db8434de60d0b3c95fa1ebcc3b3
cassiewatsonn <121472771+cassiewatsonn@users.noreply.github.com>
2023-02-02 14:06:01 -0500

    styled text for randomized radio station. working on centering picture of radio station

--- assets/js/script.js
+++ assets/js/script.js
@@ -338,7 +338,7 @@ countries.forEach(country => {
 });
 
 
-
+  
 
 ///When the use clicks the Menu button, open the modal
 function openModal() {
@@ -363,6 +363,93 @@ select.addEventListener("change", function () {
 
 const createCountryUrls = () => {
   
+  // call this function then get config
+  get_radiobrowser_base_url_random().then((x) => {
+    url = `${x}/json/stations/bycountrycodeexact/${selectedCountry}`
+    // let url = `${x}/json/stations/bylanguage/${languageSelected}`
+    // url = `${x}/json/tags`
+    // url = `${x}/json/stations/bytag/${blues}`
+    radio(url);
+    return get_radiobrowser_server_config(x);
+    }).then(config => {
+    console.log("config:", config);
+    });
+  /*
+  It is not possible to do a reverse DNS from a browser yet.
+  The first part (a normal dns resolve) could be done from a browser by doing DOH (DNS over HTTPs)
+  to one of the providers out there. (google, quad9,...)
+  So we have to fallback to ask a single server for a list.
+  */
+  // Ask a specified server for a list of all other server.
+  function get_radiobrowser_base_urls() {
+    return new Promise((resolve, reject) => {
+      var request = new XMLHttpRequest()
+      // If you need https, please use the fixed server fr1.api.radio-browser.info for this request only
+      request.open('GET', 'http://all.api.radio-browser.info/json/servers', true);
+      request.onload = function () {
+        if (request.status >= 200 && request.status < 300) {
+          var items = JSON.parse(request.responseText).map(x => "https://" + x.name);
+          console.log('server list:', items)
+          resolve(items);
+        } else {
+          reject(request.statusText);
+        }
+      }
+      request.send();
+    });
+  }
+
+  // Ask a server for its settings.
+  function get_radiobrowser_server_config(baseurl) {
+    return new Promise((resolve, reject) => {
+      var request = new XMLHttpRequest()
+      request.open('GET', baseurl + '/json/config', true);
+      request.onload = function () {
+        if (request.status >= 200 && request.status < 300) {
+          var items = JSON.parse(request.responseText);
+          resolve(items);
+        } else {
+          reject(request.statusText);
+        }
+      }
+      request.send();
+    });
+  }
+
+  /*
+  Get a random available radio-browser server.
+  Returns: string - base url for radio-browser api
+  */
+  function get_radiobrowser_base_url_random() {
+    return get_radiobrowser_base_urls().then(hosts => {
+      var item = hosts[Math.floor(Math.random() * hosts.length)];
+      return item;
+    });
+  }
+
+  // radio player function
+  function radio(url) {
+    fetch(url, {
+      method: 'GET',
+    })
+      .then(response => response.json())
+      .then(data => {
+        let ranRadio = randomNum(data.length)
+        let selectedRadio = data[ranRadio];
+        if (selectedRadio.ssl_error === 0 && selectedRadio.codec === 'MP3') {
+          $('#audio').attr('src', data[ranRadio].url);
+          console.log('radio obj:', data[ranRadio]);
+          console.log('homepage:', data[ranRadio].homepage);
+          radioData = data[ranRadio];
+          console.log(radioData);
+          displayRadioInfo();
+        } else {
+          console.log(`Radio Station "${selectedRadio.name} is offline"`);
+          radio(url)
+        }
+      })
+  }
+
 };
 
 /*
@@ -547,4 +634,4 @@ $("#ranBtn").click(function () {
 setInterval(function () {
   $('#clock').text(dayjs().format('hh:mm:ss a'));
   // console.log(dayjs().format('hh:mm:ss a'))
-}, 1000);
+}, 1000)
